877950dde8f2848b9c2c469f0061b2fee57c51c1
Taehoon Kim <taehoon-a.kim@rohde-schwarz.com>
2022-05-09 22:28:21 +0900

    [feature/issue-23] enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.enableCors();
+  // app.enableCors({
+  //   origin: true,
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
+
   await app.listen(port);
 }
 bootstrap();
